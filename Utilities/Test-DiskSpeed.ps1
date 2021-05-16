1d2f41e3a97d235d8912c29f9b3ac1e1327fb094
Daniel Möll <daniel.moll@outlook.com>
2021-05-16 13:56:01 +0200

    Various additions and changes.

--- Utilities/Test-DiskSpeed.ps1
+++ Utilities/Test-DiskSpeed.ps1
@@ -18,18 +18,31 @@ function Test-DiskSpeed {
     param(
         [Parameter(
             Mandatory
-            , HelpMessage = "Enter the designated letter of the drive which will be tested, ex: 'C'"
+            , HelpMessage = 'Enter the designated letter of the drive to be tested, ex: C'
         )]
         [ValidatePattern("[a-zA-Z]")]
         [string]$DriveLetter,
+        [Parameter(
+            HelpMessage = 'Enter a number between 0 and 100. Default is 50, 0 equals 100% reads.'
+        )]
         [ValidatePattern("^([0-9]|[1-9][0-9]|100)$")]
         [Int]$WritePercentage = 50,
-        [ValidatePattern("^([0-9]|[1-9][0-9]|100)$")]
+        [Parameter(
+            HelpMessage = 'Enter size of test file, ex: 2.'
+        )]
+        [String]$TestFileSize = 2, 
+        [ValidatePattern("^([0-9]|[1-9][0-9]|[1-9][0-9][0-9])$")]
+        [Parameter(
+            HelpMessage = 'Enter warm up time in seconds before main test is run. Default is 5, max is 999'
+        )]
         [Int]$WarmUpTime = 5,
+        [Parameter(
+            HelpMessage = 'Specify if the file created for the test is to be removed after the test is done. Default is $false'
+        )]
         [Bool]$RemoveTestFile = $false
     )
 
-    if ("$DriveLetter" -notin ( Get-Volume ).DriveLetter) {
+    if ( $DriveLetter -notin ( Get-Volume ).DriveLetter ) {
         Write-Error "Volume '$( $DriveLetter.ToUpper() )' not found" -ErrorAction Stop
     }
         
@@ -39,22 +52,24 @@ function Test-DiskSpeed {
     $threads = "-t{0}" -f ( Get-WmiObject win32_processor ).ThreadCount
     $writes = "-w{0}" -f $WritePercentage
     $warmup = "-W{0}" -f $WarmUpTime
+    $filesize = "-c{0}" -f $TestFileSize
     $archivepath = "C:\StorageTestArchive\{0}_{1}" -f $drive.DriveLetter, $drive.FileSystemLabel
-    $testfile = "{0}:\StorageTest\IOTestFile.dat" -f $drive.DriveLetter
+    $testfilepath = "{0}:\StorageTest\IOTestFile.dat" -f $drive.DriveLetter
     $formatresult = @(
+        $archivepath
         $hostname, 
         $drive.FileSystemLabel, 
         $drive.DriveLetter, 
         $date
     )
-    $resultfile = "$($archivepath)\io_resultfile_{0}_{1}_{2}_{3}.txt" -f $formatresult
+    $resultfilepath = "{0}\diskspd_resultfile_{1}_{2}_{3}_{4}.txt" -f $formatresult
 
     if ($true -ne (Test-Path -Path $archivepath)) {
         [void](New-Item -Path $archivepath -ItemType Directory)
     }
 
     #https://github.com/Microsoft/diskspd/wiki/Command-line-and-parameters
-    C:\DiskSpd\diskspd.exe -b8K -d15 -h -L -o32 $threads $warmup -r $writes -c4G $testfile > $resultfile
+    C:\DiskSpd\diskspd.exe -b8K -d15 -h -L -o32 $threads $warmup -r $writes $filesize $testfilepath > $resultfilepath
 
     if ( $true -eq $RemoveTestFile ) {
         Write-Verbose "Removing $testfile..."
